0eff71081d417e95a8ec8cf0c371c4b5350c7390
Hui Liang <huizliang@gmail.com>
2024-10-05 17:04:02 -0400

    Update create_new_table.sql

--- Case Study: Cyclistic/create_new_table.sql
+++ Case Study: Cyclistic/create_new_table.sql
@@ -1,5 +1,5 @@
 -- CREATE TABLE
-CREATE TABLE 2020_0607_tripdata
+CREATE TABLE 202006_tripdata
 (
 ride_id VARCHAR(255),
 rideable_type VARCHAR(255),
@@ -16,6 +16,28 @@ member_casual VARCHAR(255)
 );
 
 -- LOAD DATA INFILE
-LOAD DATA INFILE '2020_0607_tripdata.csv' INTO TABLE 2020_0607_tripdata
+LOAD DATA INFILE '202006_tripdata.csv' INTO TABLE 202006_tripdata
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
+
+-- UNION 202006_tripdata and 202007_tripdata
+SELECT *
+FROM 
+(
+SELECT *
+FROM 202006_tripdata 
+UNION ALL
+SELECT *
+FROM 202007_tripdata
+UNION ALL
+SELECT *
+)
+a
+
+-- ADD NEW COLUMN
+ALTER TABLE testing
+ADD ride_duration
+
+-- CALCULATE AND INSERT TIME DIFFERENCE INTO NEW COLUMN
+UPDATE 2020_0607_tripdata
+SET ride_duration = timestampdiff(second, started_at, end_at)
