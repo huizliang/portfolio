656fd64b68ec2e1f10a731b155a0e88dc49702a4
Hui Liang <huizliang@gmail.com>
2024-10-05 17:20:49 -0400

    Update queries.sql

--- Case Study: Cyclistic/queries.sql
+++ Case Study: Cyclistic/queries.sql
@@ -1,26 +1,27 @@
--- SELECT statement
+-- CREATE new column
+ALTER TABLE 2020_0607_tripdata
+ADD ride_duration int
 
--- select only rides taken by casual members
+-- UPDATE new column with time difference calculation
+UPDATE 2020_0607_tripdata
+SET ride_duration = timestampdiff(minute, started_at, end_at)
+  
+-- SELECT only rides of casual members
 
 select *
 FROM 2020_0607_tripdata
 WHERE member_casual LIKE 'c%'
 
--- select only rides taken by annual members
-select *
-FROM 2020_0607_tripdata
-WHERE member_casual LIKE 'm%'
-
--- MAX, MIN, MEAN OF ANNUAL MEMBERS
-
-SELECT avg(ride_duration)
-FROM 2020_0607_tripdata
-WHERE member_casual LIKE 'm%'
+-- MAX, MIN, MEAN 
 
 SELECT MAX(ride_duration)
 FROM 2020_0607_tripdata
 WHERE member_casual LIKE 'm%'
 
 SELECT MIN(ride_duration)
+FROM 2020_0607_tripdata
+WHERE member_casual LIKE 'm%'
+
+SELECT avg(ride_duration)
 FROM 2020_0607_tripdata
 WHERE member_casual LIKE 'm%'
